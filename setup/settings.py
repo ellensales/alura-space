df12c792aab12dbe93b0ef3dd3a976a1395ec123
ellen <ellensales@ua.pt>
2025-11-13 12:52:41 +0000

    final curso

--- setup/settings.py
+++ setup/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria'
+    'galeria.apps.GaleriaConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +122,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'setup/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
